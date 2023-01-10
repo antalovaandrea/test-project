11cf3d4eb2b79b40429f734a5b088d79ef1c8b21
Andrea Antalova <andrea.antalova21@gmail.com>
2023-01-10 11:35:24 +0100

    Change layout

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php  
     include "partials/header.php";
 ?>
-    <div class="col">
-        <div class="page-header">
+<div class="container">
+    <div class="row">
+        <div class="page-header  mt-3">
                 <h1>VERY MUCH TODO LIST</h1>
         </div>
-
         <ul class="list-group col-sm-6">
                 <li class="list-group-item">you very much to do this</li>
                 <li class="list-group-item">but also this thing</li>
         </ul>
-            
-        <form action="add-new.php" class="col-sm-6 mt-3">
+        
+        
+        <form action="add-new.php" class="col-sm-6">
             <p class="form-group">
                 <textarea class="form-control" name="message" id="text" rows="3" placeholder="watch this..."></textarea>
             </p>
@@ -19,7 +20,9 @@
                 <button class="btn btn-sm btn-danger" type="submit">add new thing</button>
             </p>
         </form>
+        
     </div>
+</div>
 <?php  
     include "partials/footer.php";
 ?>
